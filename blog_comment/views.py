d5943adeed1654a5fff25fd7f513aac751a678d2
LeadroyaL <243850435@qq.com>
2021-05-22 17:35:32 +0800

    Do not return email

--- blog_comment/views.py
+++ blog_comment/views.py
@@ -14,7 +14,7 @@ CONFIG_PASSWORD = "123"
 def get(request: HttpRequest, post_id: int) -> HttpResponse:
     ret = []
     for data in Comment.objects.filter(post_ID=post_id, is_reviewed=True):
-        d = model_to_dict(data, fields=['author', 'email', 'content', ])
+        d = model_to_dict(data, fields=['author', 'content', ])
         d['time'] = data.time.strftime('%Y-%m-%d %H:%M:%S')
         ret.append(d)
     return JsonResponse(ret, safe=False, json_dumps_params={"ensure_ascii": False})
